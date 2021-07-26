b9db1ddfe0f93a54e5d37847781a41f261c4bffb
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-07-26 16:59:48 -0600

    When added, the search container component is added to the home screen

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -3,13 +3,14 @@ import {View, Text} from 'react-native';
 
 import CovidMessage from '../../components/CovidMessage';
 import HomeMap from '../../components/HomeMap';
+import HomeSearch from '../../components/HomeSearch';
 
 export default function HomeScreen() {
   return (
     <View>
       <HomeMap />
       <CovidMessage />
-      {/* Bottom Message */}
+      <HomeSearch />
     </View>
   );
 }
