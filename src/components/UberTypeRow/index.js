2d734775dc17c7c30c3dab54ee1924c303e80c99
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-07-27 10:58:34 -0600

    When added, the Ubertype page is completely rendered

--- src/components/UberTypeRow/index.js
+++ src/components/UberTypeRow/index.js
@@ -3,22 +3,25 @@ import {View, Text, Image} from 'react-native';
 import styles from './styles';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export default function UberTypeRow() {
+export default function UberTypeRow({type}) {
+  const carImages = {
+    UberX: require('../../assets/images/UberX.jpeg'),
+    Comfort: require('../../assets/images/Comfort.jpeg'),
+    UberXL: require('../../assets/images/UberXL.jpeg'),
+  };
+
   return (
     <View style={styles.container}>
-      <Image
-        style={styles.image}
-        source={require('../../assets/images/UberX.jpeg')}
-      />
+      <Image style={styles.image} source={carImages[type.type]} />
       <View style={styles.middleContainer}>
         <Text style={styles.type}>
-          UberX <Ionicons name={'person'} size={16} />3
+          {type.type} <Ionicons name={'person'} size={16} />3
         </Text>
         <Text style={styles.time}> 8:03pm dropoff</Text>
       </View>
       <View style={styles.rightContainer}>
         <Ionicons name={'pricetag'} size={18} color={'#42d742'} />
-        <Text style={styles.price}>est. $27</Text>
+        <Text style={styles.price}>est. ${type.price}</Text>
       </View>
     </View>
   );
--- src/components/UberTypes/index.js
+++ src/components/UberTypes/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Pressable} from 'react-native';
 import styles from './styles';
 import UberTypeRow from '../UberTypeRow';
 import typesData from '../../assets/data/types';
 
 export default function UberTypes() {
+  const confirm = () => {
+    console.warn('confirm');
+  };
   return (
     <View>
       {typesData.map(type => {
-        return <UberTypeRow />;
+        return <UberTypeRow type={type} />;
       })}
+      <Pressable
+        onPress={confirm}
+        style={{
+          backgroundColor: 'black',
+          padding: 10,
+          margin: 10,
+          alignItems: 'center',
+        }}>
+        <Text style={{color: 'white', fontWeight: 'bold'}}>Confirm Uber</Text>
+      </Pressable>
     </View>
   );
 }
