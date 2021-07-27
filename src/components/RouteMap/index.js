b942e2ab8c6778d6808c4c035d0718b492e86096
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-07-27 13:59:15 -0600

    When added, routes and pins are displayed

---
+++ src/components/RouteMap/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import MapView, {Marker} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+import {GOOGLE_PLACES_API} from '@env';
+
+export default function RouteMap() {
+  const origin = {
+    latitude: 28.450627,
+    longitude: -16.263045,
+  };
+
+  const destination = {
+    latitude: 28.460127,
+    longitude: -16.269045,
+  };
+
+  return (
+    <MapView
+      style={{height: '100%', width: '100%'}}
+      initialRegion={{
+        latitude: 28.450627,
+        longitude: -16.263045,
+        latitudeDelta: 0.0222,
+        longitudeDelta: 0.0121,
+      }}>
+      <Marker coordinate={origin} title={'Origin'} />
+      <Marker coordinate={destination} title={'Destination'} />
+      <MapViewDirections
+        origin={origin}
+        destination={destination}
+        apikey={GOOGLE_PLACES_API}
+        strokeWidth={5}
+      />
+    </MapView>
+  );
+}
--- src/screens/SearchResults/index.js
+++ src/screens/SearchResults/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {View, Text, Dimensions} from 'react-native';
-import HomeMap from '../../components/HomeMap';
+import RouteMap from '../../components/RouteMap';
 import UberTypes from '../../components/UberTypes';
 
 export default function SearchResults() {
   return (
     <View>
       <View style={{height: Dimensions.get('window').height - 400}}>
-        <HomeMap />
+        <RouteMap />
       </View>
       <View style={{height: 400}}>
         <UberTypes />
